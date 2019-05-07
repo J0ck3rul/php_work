7377966c170dde152b75437e2d6e22ffdf890974
J0ck3rul <eduard.tibuleac@gmail.com>
2019-05-07 12:49:38 -0700

    rss project done

---
+++ rss_project/index.php
@@ -0,0 +1,28 @@
+<?php
+$url = "http://rss.slashdot.org/Slashdot/slashdotMain";
+
+
+$rss = new DOMDocument();
+$rss->load($url);
+
+$feed = array();
+foreach ($rss->getElementsByTagName('item') as $node) {
+    $item = array ( 
+        'title' => $node->getElementsByTagName('title')->item(0)->nodeValue,
+        'desc' => $node->getElementsByTagName('description')->item(0)->nodeValue,
+        'link' => $node->getElementsByTagName('link')->item(0)->nodeValue,
+        );
+    array_push($feed, $item);
+    echo $item['title'];
+    echo "<br>";
+    echo $item['link']; 
+}
+$limit = 5;
+for($x=0;$x<$limit;$x++)
+{
+   
+}
+// echo $rss;
+?>
+
+
